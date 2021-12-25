cd7ef94b7168502db17ad4b11c4b05a5af1300a7
Marco Boccia <pokermark95@live.it>
2021-12-25 17:10:57 +0100

    creato scheletro player e songlist

---
+++ script.js
@@ -0,0 +1,22 @@
+let tracks= [
+
+    {url:'./audio/BLANCO, Sfera Ebbasta - MI FAI IMPAZZIRE.mp3',cover:'./Photo/mi fai impazzire.webp',artist:'Blanco',title:'Mi fai impazzire feat Sferaebbasta'},
+
+    {url:'./audio/BLANCO - Notti In Bianco.mp3',cover:'./Photo/nottiinbianco.jpeg',artist:'Blanco',title:'Notti in Bianco'},
+
+    {url:'./audio/BLANCO - Lucciole (Visual).mp3',cover:'./Photo/lucciole.jpeg',artist:'Blanco',title:'Lucciole'},
+
+    {url:'./audio/BLANCO - Paraocchi.mp3',cover:'./Photo/Paraocchi.jpeg',artist:'Blanco',title:'Paraocchi'},
+
+    {url:'./audio/BLANCO - Sai Cosa C’è (Visual).mp3',cover:'./Photo/saicosac è.jpeg',artist:'Blanco',title:'Sai cosa c è'},
+
+    {url:'./audio/BLANCO - Finché Non Mi Seppelliscono (Visual).mp3',cover:'./Photo/finchè.jpeg',artist:'Blanco',title:'Finchè non mi seppelliscono'},
+
+]
+
+function populateSongList(){
+
+    console.log(tracks);
+}
+
+populateSongList()
